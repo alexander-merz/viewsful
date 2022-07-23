e66fa0f25510bfdd07ab2111531f2905ec209416
alexander-merz <web.engineering.merz@gmail.com>
2022-07-23 16:55:19 +0200

    update package.json and output formats

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
+import { name } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: 'packages/index.ts',
-      formats: ['es'],
-      fileName: 'index',
+      entry: 'packages',
+      formats: ['es', 'umd'],
+      name,
     },
-    rollupOptions: {},
   },
 })

52dd934c6e38c173fc2cee3765a99b37dc0a4c73
alexander-merz <web.engineering.merz@gmail.com>
2022-07-30 12:47:08 +0200

    docs: set up storybook

---
+++ src/components/checkbox/checkbox.stories.ts
@@ -0,0 +1,9 @@
+import { html } from 'lit-html'
+import './checkbox'
+
+export default {
+  title: 'Checkbox',
+  component: 'viewsful-checkbox',
+}
+
+export const Default = () => html`<viewsful-checkbox></viewsful-checkbox>`
---
+++ src/components/switch/switch.stories.ts
@@ -0,0 +1,9 @@
+import { html } from 'lit-html'
+import './switch'
+
+export default {
+  title: 'Switch',
+  component: 'viewsful-switch',
+}
+
+export const Default = () => html`<viewsful-switch></viewsful-switch>`

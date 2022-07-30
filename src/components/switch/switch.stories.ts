66fcebd3189f478983db198b994ed8245a7e278f
alexander-merz <web.engineering.merz@gmail.com>
2022-07-30 13:04:25 +0200

    docs: showcase css properties usage in storybook

--- src/components/switch/switch.stories.ts
+++ src/components/switch/switch.stories.ts
@@ -1,4 +1,5 @@
 import { html } from 'lit-html'
+import { styleMap } from 'lit/directives/style-map.js'
 import './switch'
 
 export default {
@@ -7,3 +8,10 @@ export default {
 }
 
 export const Default = () => html`<viewsful-switch></viewsful-switch>`
+export const Large = () => {
+  const styles = {
+    '--switch-size': '2rem',
+  }
+
+  return html`<viewsful-switch style=${styleMap(styles)}></viewsful-switch>`
+}

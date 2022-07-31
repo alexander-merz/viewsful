c42e9112545fc5edaa49861e713dae1beb00d90d
alexander-merz <web.engineering.merz@gmail.com>
2022-07-31 13:38:41 +0200

    docs: add first control for css property

--- src/components/switch/switch.stories.ts
+++ src/components/switch/switch.stories.ts
@@ -1,17 +1,17 @@
 import { html } from 'lit-html'
-import { styleMap } from 'lit/directives/style-map.js'
+import { StyleInfo, styleMap } from 'lit/directives/style-map.js'
 import './switch'
 
 export default {
   title: 'Switch',
   component: 'viewsful-switch',
+  argTypes: {
+    '--switch-size': {
+      control: { type: 'text' },
+    },
+  },
 }
 
-export const Default = () => html`<viewsful-switch></viewsful-switch>`
-export const Large = () => {
-  const styles = {
-    '--switch-size': '2rem',
-  }
-
-  return html`<viewsful-switch style=${styleMap(styles)}></viewsful-switch>`
+export const Customize = (styleInfo: Readonly<StyleInfo>) => {
+  return html`<viewsful-switch style=${styleMap(styleInfo)}></viewsful-switch>`
 }
